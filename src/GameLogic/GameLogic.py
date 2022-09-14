c62bad09e8e73df2e3c96fe5c47defed5e19682d
Robert Ludwig <robert@krautnerds.de>
2022-09-14 16:38:42 +0200

    Use grid_n_fields modul

--- src/GameLogic/GameLogic.py
+++ src/GameLogic/GameLogic.py
@@ -1,6 +1,6 @@
 import time
-
-from GameLogic.GameGrid import GameGrid
+from grid_n_fields import Grid, FieldContext as FC
+# from GameLogic.GameGrid import GameGrid
 from GameLogic.StatusData import StatusData
 
 
@@ -10,7 +10,7 @@ class GameLogic:
         self.difficulty_map = {1: 0.14, 2: 0.17, 3: 0.20, 4: 0.23, 5: 0.26}
         percentage_of_mines = self.difficulty_map[difficulty]
 
-        self.game_grid = GameGrid(y_size, x_size, percentage_of_mines)
+        self.game_grid = Grid(y_size, x_size, percentage_of_mines)
         self.statusData = StatusData(self.game_grid.mine_count)
 
         self.fields_to_open = set()
@@ -25,88 +25,82 @@ class GameLogic:
         self.cheat = False
         self.pause = False
 
-    def first_click(self, y, x):
+    def first_click(self, coordinates):
         self.statusData.start_time = time.time()
-        self.game_grid.set_mines_data(y, x)
-        self.click_closed_field(y, x)
+        self.game_grid.set_mine_data(coordinates)
+        self.click_closed_field(coordinates)
         self.first = False
 
     # Is called on click of a field
-    def click_closed_field(self, y, x):
-        field = self.game_grid.get_field_with_coordinates((y, x))
-        if not field.get_flag():
-            self._check_field_for_mine_and_open_it(field)
-
-    def click_open_field(self, y, x):
-        field = self.game_grid.get_field_with_coordinates((y, x))
-        if field.get_number() != 0:
-            if self._flag_count(y, x) == field.get_number():
-                self._open_closed_neighbors(x, y)
-
-    def _open_closed_neighbors(self, x, y):
-        neighbors = self.game_grid.neighbors_of_coordinates(y, x)
-        for t in neighbors:
-            field = self.game_grid.get_field_with_coordinates(t)
-            if not field.get_open() and not field.get_flag():
+    def click_closed_field(self, coordinates):
+        with FC(self.game_grid, coordinates) as field:
+            if not field.is_flag:
                 self._check_field_for_mine_and_open_it(field)
 
+    def click_open_field(self, coordinates):
+        with FC(self.game_grid, coordinates) as field:
+            if field.number != 0:
+                if self._flag_count(coordinates) == field.number:
+                    self._open_closed_neighbors(coordinates)
+
+    def _open_closed_neighbors(self, coordinates):
+        neighbors = self.game_grid.neighbors_of_coordinates(coordinates)
+        for t in neighbors:
+            with FC(self.game_grid, t) as field:
+                if not field.is_open and not field.is_flag:
+                    self._check_field_for_mine_and_open_it(field)
+
     def _check_field_for_mine_and_open_it(self, field):
-        if not field.get_mine():
+        if not field.is_mine:
             self._open_field_and_check_neighbors(field)
             self._process_fields_to_open()
         else:
             self.loose = True
 
-    def _flag_count(self, y, x):
-        neighbors = self.game_grid.neighbors_of_coordinates(y, x)
+    def _flag_count(self, coordinates):
+        neighbors = self.game_grid.neighbors_of_coordinates(coordinates)
         flags = 0
         for t in neighbors:
-            field = self.game_grid.get_field_with_coordinates(t)
-            if field.get_flag():
-                flags += 1
+            with FC(self.game_grid, t) as field:
+                if field.is_flag:
+                    flags += 1
         return flags
  
     def _open_field_and_check_neighbors(self, field):
-        y, x = field.get_coordinates()
-        field.set_open(True)
-        self.open_fields.add((y, x))
-        neighbors = self._add_field_and_neighbors_to_render_list(field)
-        if field.get_number() == 0:
+        coordinates = field.coordinates
+        field.is_open = True
+        self.open_fields.add(coordinates)
+        neighbors = self._add_field_and_neighbors_to_render_list(coordinates)
+        if field.number == 0:
             self._add_closed_neighbors_to_fields_to_open(neighbors)
 
     def _add_closed_neighbors_to_fields_to_open(self, neighbors):
-        for tup in neighbors:
-            field = self.game_grid.get_field_with_coordinates(tup)
-            if not field.get_open():
-                self.fields_to_open.add(field)
+        for t in neighbors:
+            with FC(self.game_grid, t) as field:
+                if not field.is_open:
+                    self.fields_to_open.add(t)
 
     def _process_fields_to_open(self):
         while self.fields_to_open:
-            i_field = self.fields_to_open.pop()
-            self._open_field_and_check_neighbors(i_field)
+            coordinates = self.fields_to_open.pop()
+            with FC(self.game_grid, coordinates) as field:
+                self._open_field_and_check_neighbors(field)
 
-    def add_field_to_render_list(self, coordinates):
-        field = self.game_grid.get_field_with_coordinates(coordinates)
-        self.render_list.add(field)
+    def flag_field(self, coordinates):
+        with FC(self.game_grid, coordinates) as field:
+            if not field.is_open:
+                if not field.is_flag:
+                    field.is_flag = True
+                    self.statusData.remaining_mines -= 1
+                else:
+                    field.is_flag = False
+                    self.statusData.remaining_mines += 1
+                self._add_field_and_neighbors_to_render_list(coordinates)
 
-    def flag_field(self, y, x):
-        field = self.game_grid.get_field_with_coordinates((y, x))
-        if not field.get_open():
-            if not field.get_flag():
-                field.set_flag(True)
-                self.statusData.remaining_mines -= 1
-            else:
-                field.set_flag(False)
-                self.statusData.remaining_mines += 1
-            self._add_field_and_neighbors_to_render_list(field)
-
-    def _add_field_and_neighbors_to_render_list(self, field):
-        self.render_list.add(field)
-        y, x = field.get_coordinates()
-        neighbors = self.game_grid.neighbors_of_coordinates(y, x)
-        for tup in neighbors:
-            tup_field = self.game_grid.get_field_with_coordinates(tup)
-            self.render_list.add(tup_field)
+    def _add_field_and_neighbors_to_render_list(self, coordinates):
+        self.render_list.add(coordinates)
+        neighbors = self.game_grid.neighbors_of_coordinates(coordinates)
+        self.render_list = self.render_list | neighbors
         return neighbors
 
     def check_win(self):
--- src/MineWindow.py
+++ src/MineWindow.py
@@ -3,6 +3,7 @@ import curses
 from SuperWin import SuperWin
 from StatusWindow import StatusWindow
 from PauseWin import PauseWin
+from grid_n_fields import FieldContext as FC
 
 
 class MineWindow(SuperWin):
@@ -15,7 +16,7 @@ class MineWindow(SuperWin):
         self.status = StatusWindow(self.manager.s_win, self.context)
         self.pause_win = PauseWin(self.manager.p_win, self.manager, self.context)
         self.max_y, self.max_x = self.scr.getmaxyx()
-        self.cursor_y, self.cursor_x = (self.max_y // 2 - 1, 2)
+        self.cursor_y, self.cursor_x = (1, 2)
         self.color = Colors()
         self.x_index = self.cursor_x // 2
         self.closed_field = '*'
@@ -30,7 +31,7 @@ class MineWindow(SuperWin):
         self.update_index()
         self.scr.box()
         self.scr.addstr(self.cursor_y, self.cursor_x, self.closed_field, curses.color_pair(12) | curses.A_REVERSE)
-        self.logic.add_field_to_render_list((self.cursor_y, self.x_index))
+        self.logic.render_list.add((self.cursor_y, self.x_index))
 
     def render_mine_win(self):
         if self.logic.loose:
@@ -45,70 +46,72 @@ class MineWindow(SuperWin):
                     self.scr.addstr(0, int(self.max_x / 2 - 6), ' cheater >.> ')
 
     def render_fields_from_render_list(self):
-        for field in self.logic.render_list:
-            logic_coordinates = field.get_coordinates()
-            y_pos, x_pos = field.get_render_coordinates()
-            if logic_coordinates in self.logic.game_grid.boarder:
+        for coordinates in self.logic.render_list:
+            if coordinates in self.logic.game_grid.boarder:
                 continue
-            if not (y_pos, x_pos) == (self.cursor_y, self.cursor_x):
-                style = curses.A_NORMAL
-            else:
-                style = curses.A_REVERSE
+            with FC(self.logic.game_grid, coordinates) as field:
+                y_pos, x_pos = field.render_coordinates
+                if not (y_pos, x_pos) == (self.cursor_y, self.cursor_x):
+                    style = curses.A_NORMAL
+                else:
+                    style = curses.A_REVERSE
 
-            if field.get_open():
-                field.set_relevant(self.logic.game_grid.is_relevant_open_field(logic_coordinates))
-            else:
-                field.set_relevant(self.logic.game_grid.is_relevant_closed_field(logic_coordinates))
+                if field.is_open:
+                    field.is_relevant = self.logic.game_grid.is_relevant_open_field(coordinates)
+                else:
+                    field.is_relevant = self.logic.game_grid.is_relevant_closed_field(coordinates)
 
-            self.scr.addstr(y_pos, x_pos, field.get_current_symbol(),
-                            curses.color_pair(field.get_current_color_id()) | style)
+                self.scr.addstr(y_pos, x_pos, field.symbol,
+                                curses.color_pair(field.color_id) | style)
         self.scr.box()
         self.logic.render_list.clear()
 
     def render_loose(self):
-        for row in self.logic.game_grid.grid:
-            for field in row:
-                y_pos, x_pos = field.get_render_coordinates()
-                if field.get_coordinates() in self.logic.game_grid.boarder:
+        for y in range(self.logic.game_grid.y_size):
+            for x in range(self.logic.game_grid.x_size):
+                if (y, x) in self.logic.game_grid.boarder:
                     continue
-                # colors all flags
-                if field.get_flag():
-                    # correct flag
-                    if field.get_mine():
-                        self.scr.addstr(y_pos, x_pos, self.flag_field, curses.color_pair(11))
-                    # incorrect flag
+                with FC(self.logic.game_grid, (y, x)) as field:
+                    y_pos, x_pos = field.render_coordinates
+                    # colors all flags
+                    if field.is_flag:
+                        # correct flag
+                        if field.is_mine:
+                            self.scr.addstr(y_pos, x_pos, self.flag_field, curses.color_pair(11))
+                        # incorrect flag
+                        else:
+                            self.scr.addstr(y_pos, x_pos, self.flag_field, curses.color_pair(9))
+                        continue
+                    # color empty fields
+                    if field.number == 0:
+                        self.scr.addstr(y_pos, x_pos, ' ')
+                    # colors all closed mines
+                    elif field.number == 9:
+                        self.scr.addstr(y_pos, x_pos, self.closed_field, curses.color_pair(10))
+                    # color all fields with numbers on
                     else:
-                        self.scr.addstr(y_pos, x_pos, self.flag_field, curses.color_pair(9))
-                    continue
-                # color empty fields
-                if field.get_number() == 0:
-                    self.scr.addstr(y_pos, x_pos, ' ')
-                # colors all closed mines
-                elif field.get_number() == 9:
-                    self.scr.addstr(y_pos, x_pos, self.closed_field, curses.color_pair(10))
-                # color all fields with numbers on
-                else:
-                    field_num = field.get_number()
-                    self.scr.addstr(y_pos, x_pos, str(field_num), curses.color_pair(field_num))
+                        field_num = field.number
+                        self.scr.addstr(y_pos, x_pos, str(field_num), curses.color_pair(field_num))
         # colors cursor position
-        curs_field = self.logic.game_grid.get_field_with_coordinates((self.cursor_y, self.x_index))
-        # on mine
-        if curs_field.get_mine():
-            self.scr.addstr(self.cursor_y, self.cursor_x, self.explode_field, curses.color_pair(9))
-        else:
-            self.scr.addstr(self.cursor_y, self.cursor_x, str(curs_field.get_number()), curses.color_pair(9))
+        with FC(self.logic.game_grid, (self.cursor_y, self.x_index)) as curs_field:
+            # on mine
+            if curs_field.is_mine:
+                self.scr.addstr(self.cursor_y, self.cursor_x, self.explode_field, curses.color_pair(9))
+            else:
+                self.scr.addstr(self.cursor_y, self.cursor_x, str(curs_field.number), curses.color_pair(9))
 
         self.scr.refresh()
         self.scr.box()
 
     def reset_render(self):
-        for row in self.logic.game_grid.grid:
-            for field in row:
-                y_pos, x_pos = field.get_render_coordinates()
-                if field.get_coordinates() in self.logic.game_grid.boarder:
+        for y in range(self.logic.game_grid.y_size):
+            for x in range(self.logic.game_grid.x_size):
+                if (y, x) in self.logic.game_grid.boarder:
                     continue
-                self.scr.addstr(y_pos, x_pos, field.get_current_symbol(),
-                                curses.color_pair(field.get_current_color_id()))
+                with FC(self.logic.game_grid, (y, x)) as field:
+                    y_pos, x_pos = field.render_coordinates
+                    self.scr.addstr(y_pos, x_pos, field.symbol,
+                                    curses.color_pair(field.color_id))
         self.scr.box()
 
     def x_start(self):
@@ -126,7 +129,7 @@ class MineWindow(SuperWin):
             
     def add_current_courser_pos_to_render(self):
         self.update_index()
-        self.logic.add_field_to_render_list((self.cursor_y, self.x_index))
+        self.logic.render_list.add((self.cursor_y, self.x_index))
 
     def up_input(self):
         if not (self.logic.loose or self.logic.win):
@@ -159,17 +162,19 @@ class MineWindow(SuperWin):
     def click_input(self):
         if not (self.logic.loose or self.logic.win):
             self.logic.update_last_game_state()
+            cursor_coordinates = (self.cursor_y, self.x_index)
             if self.logic.first:
-                self.logic.first_click(self.cursor_y, self.x_index)
+                self.logic.first_click(cursor_coordinates)
             else:
-                if not self.logic.game_grid.grid[self.cursor_y][self.x_index].get_open():
-                    self.logic.click_closed_field(self.cursor_y, self.x_index)
-                else:
-                    self.logic.click_open_field(self.cursor_y, self.x_index)
+                with FC(self.logic.game_grid, cursor_coordinates) as field:
+                    if not field.is_open:
+                        self.logic.click_closed_field(cursor_coordinates)
+                    else:
+                        self.logic.click_open_field(cursor_coordinates)
 
     def flag_input(self):
         if not (self.logic.loose or self.logic.win or self.logic.first):
-            self.logic.flag_field(self.cursor_y, self.x_index)
+            self.logic.flag_field((self.cursor_y, self.x_index))
 
     def reset_input(self):
         if self.logic.loose:
